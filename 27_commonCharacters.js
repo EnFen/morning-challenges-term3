bc249365bd6593e3b1d5134347638b5823f05569
Nathan <nathan.fenimore@gmail.com>
2019-01-14 08:45:12 +1000

    updated common characters

--- 27_commonCharacters.js
+++ 27_commonCharacters.js
@@ -11,14 +11,46 @@ commonCharacters("Alright alright alright", "") => ""
 */
 
 const commonCharacters = (string1, string2) => {
-    //    Your code here
+    // let result = ''
+    // for (char1 of string1) {
+    //     if (char1 === ' ') {
+    //         continue
+    //     }
+    //     for (char2 of string2) {
+    //         if (char2 === ' ') {
+    //             continue
+    //         } else if (char1 === char2) {
+    //             result += char1
+    //             continue
+    //         }
+    //     }
+    // }
+    // return [...new Set(result)].join('')
 
+    // return [...new Set(string1)].filter((char) => [...new Set(string2)].includes(char) && char !== ' ').join('')
+
+    // Linear time
+    string1 = string1.replace(/ /g, '')
+    string2 = string2.replace(/ /g, '')
+    let hash1 = {},
+        hash2 = {},
+        common_char = ""
+    for (char1 of string1)
+        hash1[char1] = 1
+    for (char2 of string2)
+        hash2[char2] = 1
+    for (char in hash1)
+        if (hash1[char] === hash2[char]) common_char += char
+    return common_char
 }
+
+// console.log(commonCharacters("What is love?", "Baby don't hurt me"))
+
 
 // Your Code Should Pass:
 const chai = require('chai');
 const should = chai.should();
-    
+
 describe('Common Characters', () => {
     it('returns commons characters', () => {
         commonCharacters('abc', 'abc').should.equal('abc')
